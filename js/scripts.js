d08be0a33169cee19227646a1a42f784ca600d1b
Alex Lee <doomcat9167@gmail.com>
2017-02-17 10:30:58 -0800

    add complete functionality for survey algorithm

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+//JS BackEnd Logic
+
+
+$(function(){
+  //global variables
+  var name = "";
+  //user inputs name submit
+  $("form#name").submit(function(){
+    event.preventDefault();
+    name = $("input#name").val();
+    $(".main").hide();
+    $(".survey").show();
+  });
+    //user inputs survey submit
+  $("form#survey").submit(function(){
+    event.preventDefault();
+    var q1 = $("input:radio[name=question1]:checked").val();
+    var q2 = $("input:radio[name=question2]:checked").val();
+    var q3 = $("input:radio[name=question3]:checked").val();
+    var q4 = $("input:radio[name=question4]:checked").val();
+    var q5 = $("input:radio[name=question5]:checked").val();
+    var q6 = $("input:radio[name=question6]:checked").val();
+    //console.log(q1,q2,q3,q4,q5,q6);
+
+    //Nasty nested if statements will be here
+    if (q1 === "q1a1" && (q2 === "q2a2" || "q2a3") && (q3 === "q3a1" || "q3a2" || "q3a3") && (q4 === "q4a2" || "q4a3") && (q5 === "q5a1" || "q5a2" || "q5a3")){
+      console.log("RUBY");
+    }else if (q1 === "q1a2"){
+      console.log("java");
+    }else if(q1 === "q1a1" && (q2 === "q2a2" || "q2a1" || "q2a3") && (q3 === "q3a1" || "q3a2" || "q3a3") && (q4 === "q4a1" || "q4a3" || "q4a2") && (q5 === "q5a1")){
+      console.log("php")
+    }else if (q1 === "q1a3" && q2 === "q2a3" && (q3 === "q3a1" || "q3a2" || "q3a3") && q4 === "q4a3" && q5 === "q5a1"){
+      console.log("cs")
+    }else{
+      console.log("css")
+    }
+    $(".survey").hide();
+    $("span#name").text(name);
+    $(".result").show();
+  });
+});
